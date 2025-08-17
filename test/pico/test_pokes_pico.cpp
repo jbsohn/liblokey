d656234326e4e4eb15201e7ccd34708234c6bda7
John Sohn <john.sohn@gmail.com>
2025-08-17 17:19:10 -0400

    added test_pokes_pico to play our captured data from JS7800 on the Pico

---
+++ test/pico/test_pokes_pico.cpp
@@ -0,0 +1,26 @@
+#include <cstdio>
+#include "audio/pico_pwm_audio_sink.hpp"
+#include "pokey/prosystem_pokey.hpp"
+#include "test_pokey_trace.hpp"
+#include "pico/bootrom.h"
+#include "pico/stdio.h"
+
+int main() {
+    constexpr int bufferSize = 16;
+    constexpr int sampleRate = 22050;
+
+    stdio_init_all();
+    printf("starting audio sink...\n");
+    PicoPwmAudioSink sink(0, 8u, sampleRate, bufferSize, 8);
+    sink.start();
+
+    printf("starting pokey...\n");
+    ProSystemPokey pokey(sampleRate);
+
+    for (auto event : pokeyEvents) {
+        pokey.poke(static_cast<PokeyRegister>(event.reg), event.val);
+        sink.writeAudio(pokey.renderAudio());
+    }
+    sink.stop();
+    reset_usb_boot(0, 0);
+}
